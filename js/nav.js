00fc7218776cba24f4580e22444e0b123618740b
c-fff <1208120615@qq.com>
2019-04-25 06:55:49 +0800

    4.25-6:55

--- js/nav.js
+++ js/nav.js
@@ -1,39 +1,31 @@
 // nav中集团网站箭头的更换
 function nav_wrap(){
-    var nav_left = document.getElementById("nav-left");
+    // var nav_left = document.getElementById("nav-left");
     var arrow = document.getElementById("arrow");
-        nav_left.onclick= function nav_arrow() {
-            if(arrow.src.match("down")){
-                arrow.src="img/nav_up.png";
-            }else{
-                arrow.src="img/nav_down.png";
-            }
-        }
+    // var nav_hiddenAll = document.getElementById("nav_hiddenAll");
+    $(document).ready(function () {
+        $("#nav_hiddenAll").slideToggle(200);
+    });
+    if(arrow.src.match("down")){
+        arrow.src="img/nav_up.png";
+    }else{
+        arrow.src="img/nav_down.png";
+    }
 };
 //nav中集团网站的隐藏内容
-(function nav_hidden(){
-    $(document).ready(function () {
-        $("#nav-left").click(function () {
-            $("#nav_hiddenAll").slideToggle(200);
-        });
-    })
-})();
+
 //nav语言图片的更换
 function nav_wrapTwo() {
-    var nav_right = document.getElementById("nav-right");
-     nav_right.onclick = function nav_arrowTwo() {
+    // var nav_right = document.getElementById("nav-right");
         var arrow_two = document.getElementById("arrow_two");
+    var nav_hiddenAll = document.getElementById("nav_hiddenAll");
+    nav_hiddenAll.style.display="none";
+        $(document).ready(function () {
+             $(".nav_hiddenEntire").slideToggle(200);
+         });
         if (arrow_two.src.match("down")) {
             arrow_two.src = "img/nav_up.png";
         } else {
             arrow_two.src = "img/nav_down.png";
         }
-    }
 }
-(function nav_hidden(){
-    $(document).ready(function () {
-        $("#nav-right").click(function () {
-            $(".nav_hiddenEntire").slideToggle(200);
-        });
-    })
-})();
