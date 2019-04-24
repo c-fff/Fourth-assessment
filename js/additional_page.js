2303737514b22f2ff9e33c429afa28dd34545d6c
c-fff <1208120615@qq.com>
2019-04-24 09:51:56 +0800

    4.24-9:51

---
+++ js/additional_page.js
@@ -0,0 +1,12 @@
+//第一个下拉菜单
+function additional(){
+    $(document).ready(function () {
+            $(".glide_hidden").slideToggle(500);
+    })
+}
+//第二个下拉菜单
+function additionalT(){
+    $(document).ready(function () {
+        $(".glide_hiddenTwo").slideToggle(500);
+    })
+}
