c3869032b9035734e09c2fa094d5509537e3a8f4
c-fff <1208120615@qq.com>
2019-04-29 08:54:21 +0800

    4.29-8:54

--- js/carousel.js
+++ js/carousel.js
@@ -16,83 +16,84 @@ $(function(){
 //下一个按钮
     $('.next').click(function(){
         i++;
-        //当点击次数等于图片数量时
+        //当点击次数等于图片数量时 出现白图时
         if (i==$('.img li').length){
             //使其值为1
             i=1;
             //保证无缝连接设置其距左边位置
             $('.img').css({left:0});
         };
-        $('.img').stop().animate({
+        $('.img').animate({
             left:-i*1366
         },300);
         //设置小圆点指示
         if(i==$('.img li').length-1){
+            //如果点击到最后一张图片为其设置回到第一个样式
             $('.num li').eq(0).addClass('active').siblings().removeClass('active');
         }else{
             $('.num li').eq(i).addClass('active').siblings().removeClass('active');
         }
-    })
+    });
 //        上一个按钮
     $('.prev').click(function(){
         i--;
-        if(i==-1){
+        //回到最后一张
+        if(i == -1){
             i=$('.img li').length-2;
             $('.img').css({left:-($('.img li').length-1)*1366});
         }
-        $('.img').stop().animate({left:-i*1366},300);
-        $('.num li').eq(i).addClass('active').siblings().removeClass('active');
-    })
+        $('.img').animate({left:-i*1366},300);
+    });
 //鼠标放到圆点上图片进行切换
     $('.mun li').mouseover(function(){
         //取得图片的索引
         var _index = $(this).index();
-        $('img').stop().animate({
-         //通过点击按钮获取相应的图片
+        //通过点击按钮获取相应的图片
+        $('.img').animate({
             left:-_index*1366},150);
         //给相应的按钮添加上css
         $('.mun li').eq(_index).addClass('active').siblings().removeClass('active');
-    })
-// //    通过定时器自动播放
-//     timer=setInterval(function(){
-//         i++;
-//         //如果等于图片的张数回到第一张
-//         if (i==$('.img li').length){
-//             i=1;
-//             $('.img li').css({left:0});
-//         };
-//         //停止动画时
-//         $('.img').stop().animate({left:-i*1366},300);
-//         // 如果到第三张图片时回到第一张图片
-//         if (i==$('.img li').length-1)
-//         {
-//                $('.num li').eq(0).addClass('active').siblings().removeClass('active');
-//         }
-//            //若不到第三张时每个图片对应按钮该有的样式
-//         else{
-//                $('.num li').eq(i).addClass('active').siblings().removeClass('active');
-//         }
-//     },1000);
-// //鼠标移入时暂停动画移除时动画开始
-//     $('.carousel').hover(function(){
-//         clearInterval(timer);
-//     },function(){
-//         timer=setInterval(function(){
-//                i++;
-//                if (i==$('.img li').length) {
-//                    i=1;
-//                    $('.img').css({left:0});
-//                };
-//
-//                $('.img').stop().animate({left:-i*1366},300);
-//                if (i==$('.img li').length-1) {
-//                    $('.num li').eq(0).addClass('active').siblings().removeClass('active');
-//                }else{
-//                    $('.num li').eq(i).addClass('active').siblings().removeClass('active');
-//                }
-//            },1000)
-//        })
+    });
+//    通过定时器自动播放
+    timer=setInterval(function(){
+        i++;
+        //如果等于图片的张数回到第一张
+        if (i==$('.img li').length){
+            i=1;
+            $('.img li').css({left:0});
+        };
+        $('.img').animate({left:-i*1366},300);
 
+
+        if (i==$('.img li').length-1)
+        {
+               $('.num li').eq(0).addClass('active').siblings().removeClass('active');
+        }
+           //若不到第三张时每个图片对应按钮该有的样式
+        else{
+               $('.num li').eq(i).addClass('active').siblings().removeClass('active');
+        }
+    },2000);
+//鼠标移入时暂停动画移除时动画开始
+    $('.carousel').hover(function(){
+        clearInterval(timer);
+    },function(){
+        timer=setInterval(function(){
+               i++;
+               if (i==$('.img li').length) {
+                   i=1;
+                   $('.img').css({left:0});
+               };
+               $('.img').animate({left:-i*1366},300);
+
+
+               if (i==$('.img li').length-1) {
+                   $('.num li').eq(0).addClass('active').siblings().removeClass('active');
+               }else{
+                   $('.num li').eq(i).addClass('active').siblings().removeClass('active');
+               }
+           },2000)
+       })
 });
 
 
--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,47 @@
 // nav中集团网站箭头的更换
-function nav_wrap(){
-    // var nav_left = document.getElementById("nav-left");
-    var arrow = document.getElementById("arrow");
-    // var nav_hiddenAll = document.getElementById("nav_hiddenAll");
-    $(document).ready(function () {
-        $("#nav_hiddenAll").slideToggle(200);
-    });
-    if(arrow.src.match("down")){
-        arrow.src="img/nav_up.png";
-    }else{
-        arrow.src="img/nav_down.png";
+(function nav_wrap(){
+    //内容切换
+    var Hide = true;
+    document.getElementById("nav_hiddenAll"). style.transition="2s";
+    document.getElementById("nav-left").onclick=function() {
+        if (Hide) {
+            document.getElementById("nav_hiddenEntire").style.display = 'none';
+            document.getElementById("nav_hiddenAll").style.display = 'block';
+            Hide = !Hide;
+        } else {
+            document.getElementById("nav_hiddenAll").style.display = 'none';
+            Hide = !Hide;
+        }
+        //图片切换
+        if (arrow.src.match("down")) {
+            arrow.src = "img/nav_up.png";
+        } else {
+            arrow.src = "img/nav_down.png";
+        }
     }
-};
+})();
 //nav中集团网站的隐藏内容
 
 //nav语言图片的更换
-function nav_wrapTwo() {
-    // var nav_right = document.getElementById("nav-right");
-        var arrow_two = document.getElementById("arrow_two");
-    var nav_hiddenAll = document.getElementById("nav_hiddenAll");
-    nav_hiddenAll.style.display="none";
-        $(document).ready(function () {
-             $(".nav_hiddenEntire").slideToggle(200);
-         });
+(function nav_wrapTwo() {
+    var Hide = true;
+
+    document.getElementById("nav_hiddenEntire").style.transition = "2s";
+    document.getElementById("nav-right").onclick = function () {
+        if (Hide) {
+            document.getElementById("nav_hiddenAll").style.display = 'none';
+            document.getElementById("nav_hiddenEntire").style.display = 'block';
+            Hide = !Hide;
+        } else {
+            document.getElementById("nav_hiddenEntire").style.display = 'none';
+            Hide = !Hide;
+        }
+
+        //图片切换
         if (arrow_two.src.match("down")) {
             arrow_two.src = "img/nav_up.png";
         } else {
             arrow_two.src = "img/nav_down.png";
         }
-}
+    }
+})();
